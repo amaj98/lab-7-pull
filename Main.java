5013fd1bd26cb54e178f6bef7cdc063d877af70e
amaj98 <amaj1998@gmail.com>
2018-10-07 19:18:22 -0400

    put TA feedback at top of Main.java:

--- Main.java
+++ Main.java
@@ -1,3 +1,8 @@
+/*
+1. Dead code in model. Some conditions are never executed, which is why the orc does not behave correctly in the animation. (-1)
+2. Lacks main method. (-1)
+3. Images should be organized using enums. (-1)
+*/
 public class Main{
   public static void main(String[] args){
     Controller c = new Controller();
